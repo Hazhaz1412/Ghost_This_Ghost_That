a1ac0297f98a4f7895dccb732c6f44a274a05dd7
Hazhaz1412 <hoanghuanpham3@gmail.com>
2025-12-20 16:20:23 +0700

    feat: Intro Scene with transition

---
+++ Assets/Scripts/Transition/BlackFadeIntro.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class BlackFadeIntro : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 2f;
+    public float TimeAnimation = 1.5f;
+    
+    private float timer = 0f;
+    private bool isTrigger = false;
+
+    void Update() {
+        timer += Time.deltaTime; 
+        if (timer >= TimeAnimation && !isTrigger) {
+            isTrigger = true;
+            LoadNextScene();
+        }
+         
+        if (Input.GetMouseButtonDown(0) && !isTrigger) {
+            isTrigger = true;
+            LoadNextScene();
+        }
+    }
+    
+    public void LoadNextScene() {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    
+    IEnumerator LoadLevel(int sceneIndex) {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
